8c3232bb3ac9c57a8f665f7e40820ce94beb40e4
hiyochi <62633777+hiyochi@users.noreply.github.com>
2020-04-28 02:28:34 +0800

    modify controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   resources :records
   resources :artists
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: "home#index"
+
+  post "refresh", controller: :refresh, action: :create
+  post "signin", controller: :signin, action: :create
+  post "signup", controller: :signup, action: :create
+  delete "signin", controller: :signin, action: :destroy
 end
